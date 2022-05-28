49e0c217794a6fa527a8f49d919c5df8eb658bc7
Allan Da Silva Pessin <allan.pessin@outlook.com>
2022-05-28 10:09:50 -0300

    forwarding controller parameters to route

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function test(int $p1, int $p2) {
-      echo "a soma de $p1 + $p2 é = " .($p1 + $p2);
+      // echo "a soma de $p1 + $p2 é = " .($p1 + $p2);
+      // return view('site.test', ['p1' => $p1, 'p2' => $p2]); // array associativo
+      // return view('site.test', compact('p1', 'p2')); //compact
+      
+      return view('site.test')->with('p1', $p1)->with('p2', $p2); //with
     }
 }
